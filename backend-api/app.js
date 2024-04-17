0b0e27c959a4190dd22758b2f4be3c41f621aedf
Mayavb-128 <mayavb128@gmail.com>
2024-04-16 23:18:59 -0400

    change backend cors

--- backend-api/app.js
+++ backend-api/app.js
@@ -18,7 +18,8 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://localhost:3001'
+  'http://localhost:3001',
+  'https://skill-swap-marketplace.onrender.com/'
 ];
 
 const corsOptions = {
